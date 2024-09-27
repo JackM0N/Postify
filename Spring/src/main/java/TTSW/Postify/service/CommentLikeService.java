838831a5638f68f7040f9ede5205db3be974b8c7
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-09-27 14:47:08 +0200

    Added creating notification when liking posts or comments

--- Spring/src/main/java/TTSW/Postify/service/CommentLikeService.java
+++ Spring/src/main/java/TTSW/Postify/service/CommentLikeService.java
@@ -1,16 +1,25 @@
 package TTSW.Postify.service;
 
+import TTSW.Postify.enums.NotificationType;
+import TTSW.Postify.model.Comment;
 import TTSW.Postify.model.CommentLike;
+import TTSW.Postify.model.Notification;
 import TTSW.Postify.model.WebsiteUser;
 import TTSW.Postify.repository.CommentLikeRepository;
+import TTSW.Postify.repository.CommentRepository;
+import TTSW.Postify.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CommentLikeService {
     private final CommentLikeRepository commentLikeRepository;
     private final WebsiteUserService websiteUserService;
+    private final CommentRepository commentRepository;
+    private final NotificationRepository notificationRepository;
 
     public Boolean likeComment(Long commentId) {
         WebsiteUser currentUser = websiteUserService.getCurrentUser();
@@ -20,10 +29,21 @@ public class CommentLikeService {
             commentLikeRepository.delete(likeExists);
             return false;
         }
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new RuntimeException("Comment not found"));
         CommentLike like = new CommentLike();
-        like.setId(commentId);
+        like.setComment(comment);
         like.setUser(currentUser);
         commentLikeRepository.save(like);
+
+        Notification notification = new Notification();
+        notification.setUser(comment.getUser());
+        notification.setComment(comment);
+        notification.setCreatedAt(LocalDateTime.now());
+        notification.setNotificationType(NotificationType.COMMENT_LIKE);
+        notification.setTriggeredBy(currentUser);
+        notificationRepository.save(notification);
+
         return true;
     }
 }
--- Spring/src/main/java/TTSW/Postify/service/PostLikeService.java
+++ Spring/src/main/java/TTSW/Postify/service/PostLikeService.java
@@ -1,16 +1,25 @@
 package TTSW.Postify.service;
 
+import TTSW.Postify.enums.NotificationType;
+import TTSW.Postify.model.Notification;
+import TTSW.Postify.model.Post;
 import TTSW.Postify.model.PostLike;
 import TTSW.Postify.model.WebsiteUser;
+import TTSW.Postify.repository.NotificationRepository;
 import TTSW.Postify.repository.PostLikeRepository;
+import TTSW.Postify.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class PostLikeService {
     private final WebsiteUserService websiteUserService;
     private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
+    private final NotificationRepository notificationRepository;
 
     public Boolean likePost(Long postId){
         WebsiteUser currentUser = websiteUserService.getCurrentUser();
@@ -20,10 +29,21 @@ public class PostLikeService {
            postLikeRepository.delete(likeExists);
            return false;
         }
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post Not Found"));
         PostLike postLike = new PostLike();
-        postLike.setId(postId);
+        postLike.setPost(post);
         postLike.setUser(currentUser);
         postLikeRepository.save(postLike);
+
+        Notification notification = new Notification();
+        notification.setUser(post.getUser());
+        notification.setPost(post);
+        notification.setCreatedAt(LocalDateTime.now());
+        notification.setNotificationType(NotificationType.POST_LIKE);
+        notification.setTriggeredBy(currentUser);
+        notificationRepository.save(notification);
+
         return true;
     }
 }
