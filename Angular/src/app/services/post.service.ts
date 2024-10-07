23e38fe5ef49311372cb9e5f12fd80150137f298
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-10-07 14:45:26 +0200

    Added multimedia to posts

--- Angular/src/app/services/post.service.ts
+++ Angular/src/app/services/post.service.ts
@@ -9,10 +9,15 @@ import { Page } from '../models/page.model';
 })
 export class PostService {
   private baseUrl = 'http://localhost:8080/post';
+  private mediaUrl = 'http://localhost:8080/medium';
 
   constructor(private http: HttpClient) {}
 
   getPosts(filter: any, pageable: any): Observable<Page<PostDTO>> {
     return this.http.get<Page<PostDTO>>(`${this.baseUrl}/list`, { params: { ...filter, ...pageable } });
+  }
+
+  getPostMedia(postId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.mediaUrl}/list/${postId}`);
   }
 }
