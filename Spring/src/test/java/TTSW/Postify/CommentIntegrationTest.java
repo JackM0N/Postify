1be54dae054817d34f52843151df899ba4891023
Stanislaw <31638@uniwersytetkaliski.edu.pl>
2024-10-09 12:52:30 +0200

    Updated Comment IntegrationTest to also test notifications

--- Spring/src/test/java/TTSW/Postify/CommentIntegrationTest.java
+++ Spring/src/test/java/TTSW/Postify/CommentIntegrationTest.java
@@ -2,11 +2,19 @@ package TTSW.Postify;
 
 import TTSW.Postify.dto.CommentDTO;
 import TTSW.Postify.dto.WebsiteUserDTO;
+import TTSW.Postify.enums.NotificationType;
+import TTSW.Postify.mapper.CommentMapperImpl;
 import TTSW.Postify.mapper.WebsiteUserMapper;
+import TTSW.Postify.model.Comment;
+import TTSW.Postify.model.Notification;
+import TTSW.Postify.model.Post;
+import TTSW.Postify.model.WebsiteUser;
 import TTSW.Postify.repository.CommentRepository;
+import TTSW.Postify.repository.NotificationRepository;
 import TTSW.Postify.repository.PostRepository;
 import TTSW.Postify.repository.WebsiteUserRepository;
 import TTSW.Postify.service.CommentService;
+import TTSW.Postify.service.NotificationService;
 import TTSW.Postify.service.WebsiteUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@WithMockUser("john@example.com")
 class CommentIntegrationTest {
 
     @Autowired
@@ -44,7 +54,17 @@ class CommentIntegrationTest {
     @Autowired
     private WebsiteUserMapper websiteUserMapper;
 
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private CommentMapperImpl commentMapperImpl;
+
     @Test
+    @WithAnonymousUser
     void testGetAllCommentsForPost_Success() {
         Page<CommentDTO> comments = commentService.getAllCommentsForPost(1L, PageRequest.of(0, 10));
         assertNotNull(comments);
@@ -52,28 +72,36 @@ class CommentIntegrationTest {
     }
 
     @Test
+    @WithAnonymousUser
     void testGetAllCommentsForPost_postNotFound() {
         assertThrows(EntityNotFoundException.class, () -> commentService.getAllCommentsForPost(999L, PageRequest.of(0, 10)));
     }
 
     @Test
-    @WithMockUser("john@example.com")
     void testCreateComment_Success() {
+        WebsiteUser john = websiteUserService.getCurrentUser();
+        WebsiteUser jane = websiteUserRepository.findByUsername("jane_smith").get();
+        Post post = postRepository.findById(2L).get();
+        assertEquals(post.getUser().getUsername(), "jane_smith");
+
         CommentDTO commentDTO = new CommentDTO();
         commentDTO.setText("New comment");
-        commentDTO.setPostId(1L);
-        WebsiteUserDTO websiteUserDTO = websiteUserMapper.toDto(websiteUserRepository.findById(1L).get());
-        commentDTO.setUser(websiteUserDTO);
+        commentDTO.setPostId(post.getId());
+        commentDTO.setUser(websiteUserMapper.toDto(john));
 
         CommentDTO savedComment = commentService.createComment(commentDTO);
 
         assertNotNull(savedComment);
         assertEquals("New comment", savedComment.getText());
         assertNotNull(savedComment.getId());
+
+        // notification
+        Notification notification = notificationRepository.findByUserIdAndTriggeredByIdAndNotificationTypeAndCommentId(
+                jane.getId(), john.getId(), NotificationType.COMMENT, savedComment.getId()).get();
+        assertNotNull(notification);
     }
 
     @Test
-    @WithMockUser("john@example.com")
     void testCreateComment_NoSuchPost() {
         CommentDTO commentDTO = new CommentDTO();
         commentDTO.setText("New comment");
@@ -87,8 +115,10 @@ class CommentIntegrationTest {
     @Test
     @WithMockUser("jane@example.com")
     void testUpdateComment_Success() {
-        CommentDTO commentDTO = new CommentDTO();
-        commentDTO.setId(1L);
+        Comment comment = commentRepository.findById(1L).get();
+        assertNotNull(comment);
+        assertEquals(comment.getUser(), websiteUserService.getCurrentUser());
+        CommentDTO commentDTO = commentMapperImpl.toDto(comment);
         commentDTO.setText("Updated comment text");
 
         CommentDTO updatedComment = commentService.updateComment(commentDTO);
@@ -98,10 +128,11 @@ class CommentIntegrationTest {
     }
 
     @Test
-    @WithMockUser("johm@example.com")
     void testUpdateComment_notAuthorized() {
-        CommentDTO commentDTO = new CommentDTO();
-        commentDTO.setId(1L);
+        Comment comment = commentRepository.findById(1L).get();
+        assertNotNull(comment);
+        assertNotEquals(comment.getUser(), websiteUserService.getCurrentUser());
+        CommentDTO commentDTO = commentMapperImpl.toDto(comment);
         commentDTO.setText("Not my comment, can I update it?");
 
         assertThrows(BadCredentialsException.class, () -> commentService.updateComment(commentDTO));
@@ -110,22 +141,28 @@ class CommentIntegrationTest {
     @Test
     @WithMockUser("jane@example.com")
     void testDeleteComment_Success() {
-        commentService.deleteComment(1L);
+        Comment comment = commentRepository.findById(1L).get();
+        assertNotNull(comment);
+        commentService.deleteComment(comment.getId());
 
-        assertFalse(commentRepository.findById(1L).isPresent());
+        assertFalse(commentRepository.findById(comment.getId()).isPresent());
     }
 
     @Test
     @WithMockUser("testadmin@localhost")
     void testDeleteComment_Success_Admin() {
-        commentService.deleteComment(1L);
+        Comment comment = commentRepository.findById(1L).get();
+        assertNotNull(comment);
+        commentService.deleteComment(comment.getId());
 
         assertFalse(commentRepository.findById(1L).isPresent());
     }
 
     @Test
-    @WithMockUser("john@example.com")
     void testDeleteComment_notAuthorized() {
-        assertThrows(BadCredentialsException.class, () -> commentService.deleteComment(1L));
+        Comment comment = commentRepository.findById(1L).get();
+        assertNotNull(comment);
+        assertNotEquals(comment.getUser(), websiteUserService.getCurrentUser());
+        assertThrows(BadCredentialsException.class, () -> commentService.deleteComment(comment.getId()));
     }
 }
