84169456c8b0efa53fd63eb3d3b0358d84148d80
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-10-10 17:28:26 +0200

    Added login based navbar and turned off SSR

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular';
+  isLoggedIn = false;
+  username: string | null = null;
+
+  constructor(private jwtHelper: JwtHelperService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus(): void {
+    const token = localStorage.getItem('token');
+    
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      this.username = decodedToken?.username || null;
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+      this.username = null;
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.isLoggedIn = false;
+    this.username = null;
+    this.router.navigate(['/login']);
+  }
 }
