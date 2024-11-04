bfe7a5908c92e47d758a8fb9556bb5147e1ba991
Maja552 <mayawalczynska@gmail.com>
2024-11-04 20:18:09 +0100

    Added check if token is expired in the auth interceptor

--- Angular/src/app/services/auth.intereptor.ts
+++ Angular/src/app/services/auth.intereptor.ts
@@ -2,14 +2,25 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private jwtHelper: JwtHelperService;
+
+  constructor() {
+    this.jwtHelper = new JwtHelperService();
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (typeof window !== 'undefined') {
       const token = localStorage.getItem(environment.tokenKey);
       
       if (token) {
+        if (this.jwtHelper.isTokenExpired(token)) {
+          localStorage.removeItem(environment.tokenKey);
+          return next.handle(req);
+        }
         const cloned = req.clone({
           headers: req.headers.set('Authorization', `Bearer ${token}`)
         });
